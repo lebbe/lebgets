86fb88b025aca1a634cc55f9d6bad8d869425cc0
Lebchen <lebchen@gmail.com>
2023-04-09 12:06:59 +0200

    Fix firefox glitch where the dropdown stayed open after selecting something from it.

--- src/MultiAutocomplete/MultiAutocomplete.tsx
+++ src/MultiAutocomplete/MultiAutocomplete.tsx
@@ -7,34 +7,37 @@ type Props = {
 }
 
 export default function MultiAutocomplete({ options, values, setValues, ...props }: Props) {
-  const ref = useRef<HTMLInputElement>(null)
+  const inputElementRef = useRef<HTMLInputElement>(null)
   const selected = useRef(new Set<string>())
-
-  // A unique uuid generated with Math.random()
-  const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
+  const datalistId = useRef(Math.random().toString(36).substring(2, 12)).current
 
   useEffect(() => {
-    if (!ref.current) return
+    if (!inputElementRef.current) return // How to convince TypeScript that this never happens?
 
-    const input = ref.current
+    const input = inputElementRef.current
 
     input.addEventListener('input', function (e) {
-      // @ts-ignore
+      // @ts-ignore inputType is not in the Event interface, but it is in the InputEvent interface
       if (e.inputType === 'insertReplacementText' || e.constructor.name === 'Event') {
         if (input.value === '') return
         selected.current.add(input.value)
         setValues(Array.from(selected.current))
         input.value = ''
+
+        // Prevent firefox from opening the dropdown list again
+        async function blurAndFocus() {
+          input.blur()
+          await new Promise((resolve) => setTimeout(resolve, 0))
+          input.focus()
+        }
+
+        blurAndFocus()
       }
     })
 
     input.addEventListener('keydown', function (e) {
-      // Når bruker trykker enter eller hvis bruker har skrevet hele
-      // innholdet i nedtrekkslisten OG velger fra listen (da fyres ikke input-event)
+      // When the user presses enter or if the user has written the entire
+      // content of the dropdown list AND selects from the list (then the input-event is not fired)
       if (e.key === 'Enter' || e.constructor.name === 'Event') {
         input.value && selected.current.add(input.value)
         setValues(Array.from(selected.current))
@@ -45,8 +48,8 @@ export default function MultiAutocomplete({ options, values, setValues, ...props
 
   return (
     <>
-      <input list={uuid} {...props} ref={ref} />
-      <datalist id={uuid}>
+      <input list={datalistId} {...props} ref={inputElementRef} />
+      <datalist id={datalistId}>
         {options
           .filter((option) => !selected.current.has(option))
           .map((option) => (
